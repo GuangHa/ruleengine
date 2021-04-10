a47b610678cbb07d85026132d6136b86528abbd8
GuangHa <quocguangha@gmail.com>
2021-04-10 14:36:32 +0200

    Add option to drag and drop json file into the textfields

---
+++ DistributionPackages/Guang.RuleEngine/Resources/Public/Javascript/frontend.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function() {
+    var ruleHolder = document.getElementById('ruleHolder'),
+        dataHolder = document.getElementById('dataHolder'),
+        state = document.getElementById('status');
+
+    if (typeof window.FileReader === 'undefined') {
+        state.className = 'fail';
+    } else {
+        state.className = 'success';
+        state.innerHTML = 'File API & FileReader available';
+    }
+
+    ruleHolder.ondragover = function() {
+        this.className = 'hover form-control';
+        return false;
+    };
+    dataHolder.ondragover = function() {
+        this.className = 'hover form-control';
+        return false;
+    };
+    ruleHolder.ondragend = function() {
+        this.className = 'form-control';
+        return false;
+    };
+    dataHolder.ondragend = function() {
+        this.className = 'form-control';
+        return false;
+    };
+    ruleHolder.ondrop = function(e) {
+        this.className = 'form-control';
+        e.preventDefault();
+
+        var file = e.dataTransfer.files[0],
+            reader = new FileReader();
+        reader.onload = function(event) {
+            console.log(event.target);
+            ruleHolder.value = event.target.result;
+        };
+        console.log(file);
+        reader.readAsText(file);
+
+        return false;
+    };
+    dataHolder.ondrop = function(e) {
+        this.className = 'form-control';
+        e.preventDefault();
+
+        var file = e.dataTransfer.files[0],
+            reader = new FileReader();
+        reader.onload = function(event) {
+            console.log(event.target);
+            dataHolder.value = event.target.result;
+        };
+        console.log(file);
+        reader.readAsText(file);
+
+        return false;
+    };
+});
