3c8537a3da4c5e67eaad5716b3a8b7f648613470
GuangHa <quocguangha@gmail.com>
2021-04-27 21:39:44 +0200

    Add sqrt, join and create tests

--- DistributionPackages/Guang.RuleEngine/Tests/Unit/JsonLogicTest.php
+++ DistributionPackages/Guang.RuleEngine/Tests/Unit/JsonLogicTest.php
@@ -153,4 +153,92 @@ class JsonLogicTest extends \PHPUnit\Framework\TestCase
         $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
         self::assertEquals($expected, json_encode($output));
     }
+
+    /**
+     * Test the function with number
+     *
+     * @test
+     */
+    public function sqrtFunctionNumbers()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/sqrtFunctionNumbers.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/sqrtFunctionNumbers.json');
+        $expected = '2';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * Test the function with number from data
+     *
+     * @test
+     */
+    public function sqrtFunctionNumbersFromData()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/sqrtFunctionNumbersFromData.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/sqrtFunctionNumbersFromData.json');
+        $expected = '2';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * @test
+     */
+    public function createFunctionWithEmptyData()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/createFunctionWithEmptyData.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/createFunctionWithEmptyData.json');
+        $expected = '[{"Attribute1":"value1","Attribute2":"value2"}]';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * @test
+     */
+    public function createFunctionWithData()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/createFunctionWithData.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/createFunctionWithData.json');
+        $expected = '[{"id":1,"class":"node","type":"A"},{"Attribute1":"value1","Attribute2":"value2"}]';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * @test
+     */
+    public function createFunctionWithSameData()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/createFunctionWithSameData.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/createFunctionWithSameData.json');
+        $expected = '[{"id":1,"class":"node","type":"A"},{"id":1,"class":"node","type":"A"}]';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * @test
+     */
+    public function joinFunctionWithTwoGroups()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/joinFunctionWithTwoGroups.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/applyRules.json');
+        $expected = '[[{"Group A":[{"id":1,"class":"node","type":"A"}]},{"Group B":[{"id":2,"class":"node","type":"B"},{"id":3,"class":"node","type":"B"},{"id":4,"class":"edge","type":"B"}]}]]';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
+
+    /**
+     * @test
+     */
+    public function joinFunctionWithTwoData()
+    {
+        $rule = file_get_contents(dirname(__FILE__).'/Rules/joinFunctionWithTwoData.json');
+        $data = file_get_contents(dirname(__FILE__).'/Data/joinFunctionWithTwoData.json');
+        $expected = '[{"id":1,"class":"node","type":"A"},{"id":1,"class":"node","type":"A"}]';
+        $output = JsonLogic::apply(json_decode($rule), json_decode($data), false, true, json_decode($data));
+        self::assertEquals($expected, json_encode($output));
+    }
 }
